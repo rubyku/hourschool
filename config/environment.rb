3d546cdaf659709d24af2fadd9977c8ef0f519f8
Saranyan <saranyan13@gmail.com>
2011-11-05 00:18:16 -0500

    updates

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HourschoolV2::Application.initialize!
+
+ActionMailer::Base.default_content_type = "text/html"
