83ede0c3a548a581e086185cc310927d09cc8f12
Saranyan <saranyan13@gmail.com>
2011-11-07 23:49:45 -0600

    fav classes for the homepage. changed the numbers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,10 @@ class HomeController < ApplicationController
       redirect_to user_root_path
       #redirect_to current_user
     end
-    @fav1 = Course.find(2)
+    @fav1 = Course.find(5)
     @fav2 = Course.find(3)
     @fav3 = Course.find(4)
-    @fav4 = Course.find(5)
+    @fav4 = Course.find(6)
   end
   
   def learn
