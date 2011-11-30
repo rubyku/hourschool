2693a55ef91c44f1279a78f1078c7e0bddb59574
Ruby Ku <rubyku85@gmail.com>
2011-11-30 16:56:17 -0600

    rescue if tumblr is down

--- config/initializers/tumblr.rb
+++ config/initializers/tumblr.rb
@@ -1,3 +1,8 @@
 require 'tumblr'
-TUMUSER = Tumblr::User.new('hello@hourschool.com', 'ac4dlove')
 
+begin
+  TUMUSER = Tumblr::User.new('hello@hourschool.com', 'ac4dlove')
+rescue => ex
+  Rails.logger.error ex.message
+  Rails.logger.error("TUMBLR: could not Reach Tumblr")
+end
