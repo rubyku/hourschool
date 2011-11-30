657480a0f07abc6c7bdcbc03d80d3686711ce7cd
schneems <richard.schneeman@gmail.com>
2011-11-30 11:50:18 -0600

    whitespace

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,36 +1,36 @@
 class UserMailer < ActionMailer::Base
   default :from => "HourSchool <hello@hourschool.com>"
-  
+
   def contact_all_students(subject,message,course)
     @course = course
     mail(:to => course.students.map(&:email), :subject => subject)
   end
-  
+
   def contact_teacher(subject, message, course)
     @course = course
     mail(:to => course.teacher.email, :subject => subject)
   end
-  
+
   def send_registration_mail(user_email,user_name)
     @email = user_email
     @name = user_name
     mail(:to => user_email, :subject => "Welcome to HourSchool!")
   end
-  
+
   def send_course_approval_mail(user_email, user_name, course)
     @email = user_email
     @name = user_name
     @course = course
     mail(:to => user_email, :subject => "Your proposal has been approved!")
   end
-  
+
   def send_course_registration_mail(user_email, user_name, course)
      @email = user_email
      @name = user_name
      @course = course
      mail(:to => user_email, :subject => "You have registered for a course!")
    end
-   
+
    def send_nominate_mail_to_teacher(user_email, current_user, csuggid, message)
       @email = user_email
       @user = current_user
@@ -39,5 +39,5 @@ class UserMailer < ActionMailer::Base
       @message = message
       mail(:to => user_email, :subject => "#{@user.name} has nominated you to teach at HourSchool")
     end
-  
+
 end
