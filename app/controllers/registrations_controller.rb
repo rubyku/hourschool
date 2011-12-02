53d1d92bac8425689db8c559d0eddaa2bc4ad472
schneems <richard.schneeman@gmail.com>
2011-12-02 12:18:34 -0600

    comment out code for after_sign_up_path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   # Over-riding update, all other actions are handled by Devise::RegistrationsController (the parent)
   # If you're calling any other action you can leave it blank, or call super on it
 
-  def new
-    super
-  end
 
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
@@ -18,4 +15,9 @@ class RegistrationsController < Devise::RegistrationsController
       respond_with_navigational(resource){ render_with_scope :edit }
     end
   end
+
+  protected
+    # def after_sign_up_path_for(resource)
+    #   user_root_path # after we create a user, send them to /learn
+    # end
 end
