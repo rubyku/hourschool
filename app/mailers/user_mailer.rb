b5a5fbf4320bf6c4b703f05928452a7a55ed69f6
Michael May <curiax8@gmail.com>
2011-12-02 19:01:32 -0600

    fix for email students in course

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default :from => "HourSchool <hello@hourschool.com>"
 
-  def contact_all_students(current_user,message,course)
+  def contact_all_students(current_user,course,message)
     @course = course
     @message = message
     mail(:to => course.students.map(&:email), :subject => "Your teacher sent you a message!")
