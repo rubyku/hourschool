1ae027186673559837a5ae93bfb474b8e791294e
Ruby Ku <rubyku85@gmail.com>
2011-12-03 14:17:17 -0600

    style for profile notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,34 @@ class UsersController < ApplicationController
       #p current_user
     end
   end
-
+  
   def profile
     @user = current_user
     @approved_courses = @user.courses.where(:status => "approved")
     @pending_courses = @user.courses.where(:status => "proposal")
   end
 
+  def has_notifications
+    @user = current_user
+    @approved_courses.any? || @pending_courses.count != 0 || !current_user.photo.exists?
+  end
+  
+  def has_approved_courses
+    @user = current_user
+    @approved_courses = @user.courses.where(:status => "approved")
+    @approved_courses != 0
+  end
+  
+  def has_pending_courses
+    @user = current_user
+    @pending_courses = @user.courses.where(:status => "proposal")
+    @pending_courses.count !=0
+  end
+  
+  def no_photo
+    !current_user.photo.exists?
+  end
+  
   def profile_past
   end
 
