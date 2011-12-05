eb005914e7d714c5e4509b114b6db0d006fc8671
schneems <richard.schneeman@gmail.com>
2011-12-04 21:55:54 -0600

    factories and seeds

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,50 @@
+
+
 FactoryGirl.define do
-  factory :user
+  sequence :email do |n|
+    "person_#{Time.now.to_i }@example.com"
+  end
+
+  sequence :class_name do
+    hour_markov ||= %W{ have make a grow build community freedom
+                        design programming color technique tables
+                        chart grid salsa cook love you can today
+                        easy quick sushi core work-out blast quick
+                        fun viable scale fly to monster garden run
+                        plant fish seed sustainable green natural}
+    hour_markov.shuffle.first(rand(5)+3).map(&:capitalize).join(' ')
+  end
+
+
+  factory :role do
+    user
+    course
+    role  { ['teacher', 'student'].sample }
+  end
+
+
+  factory :user do
+    name      { "#{Random.firstname}  #{Random.lastname}"}
+    email     { FactoryGirl.generate(:email) }
+    zip       { Forgery(:address).zip }
+    location  { "#{Forgery(:address).city}, #{Forgery(:address).state}" }
+
+    password  { Forgery(:basic).password }
+    bio       { Forgery(:lorem_ipsum).words(rand(10)) }
+  end
+
+
+
   factory :course do
-    title       { Forgery(:lorem_ipsum).words(rand(4) + 1) }
+    title       { FactoryGirl.generate(:class_name) }
     description { Forgery(:lorem_ipsum).words(rand(30) + 1) }
     status      "live"
-    place_name       "who ville"
-    time_range  {rand(5).to_s}
+    place_name  { Forgery::Name.location }
+    time_range  {c = rand(5); "#{c} - #{c + rand(2)}}"}
     price       { rand(100) }
-    max_seats       { rand(10) }
+    max_seats   { rand(10) }
     time        3.weeks.from_now
     date        3.weeks.from_now
-    min_seats     { rand(5) }
+    min_seats   { rand(5) }
   end
 end
