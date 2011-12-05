798a3df517d8ff1acf70ca91e1238b3ffd3e22f5
Ruby Ku <rubyku85@gmail.com>
2011-12-04 22:28:57 -0600

    removed random course function in csuggestion controller suggest def

--- app/controllers/csuggestions_controller.rb
+++ app/controllers/csuggestions_controller.rb
@@ -14,14 +14,14 @@ class CsuggestionsController < ApplicationController
            :limit => 100,
            :order => "csuggestions.name ASC"
        })
-      @suggestions = (@top_suggestions & @suggestions_in_my_location).paginate(:page => params[:page], :per_page => 6)
-      if Course.count > 0
-         @random_course = Course.random
-
-         @classes_we_like = Course.random.first(3)
-       else
-         @classes_we_like = []
-       end
+      # @suggestions = (@top_suggestions & @suggestions_in_my_location).paginate(:page => params[:page], :per_page => 6)
+      # if Course.count > 0
+      #    @random_course = Course.random
+      # 
+      #    @classes_we_like = Course.random.first(3)
+      #  else
+      #    @classes_we_like = []
+      #  end
   end
 
   def show
