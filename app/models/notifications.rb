047731c43d6d208f98b69a6f3ebe11d0761aaa33
Ruby Ku <rubyku85@gmail.com>
2011-12-04 19:00:04 -0600

    removed pending method in notification table

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -4,14 +4,4 @@ class Notifications < ActiveRecord::Base
   attr_accessible :alert, :action
 
 
-  def pending_update
-    @user = current_user
-    @approved_courses = @user.courses.where(:status => "approved")
-
-    if @approved_courses.last.description.blank?
-      "Please update your profile"
-    end
-
-  end
-
 end
