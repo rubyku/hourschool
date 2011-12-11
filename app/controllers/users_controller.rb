43a178fcb1bd77ec0bfb0f988a2e8904869437dd
schneems <richard.schneeman@gmail.com>
2011-12-11 15:51:50 -0600

    whitespace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.me_or_find(params[:id], current_user)
-    # render :text => @user.name
     @approved_courses = @user.courses.where(:status => "approved")
-    @pending_courses = @user.courses.where(:status => "proposal")
+    @pending_courses  = @user.courses.where(:status => "proposal")
   end
 
   def profile_teaching
     @user = current_user
     @approved_courses = @user.courses.where(:status => "approved")
-    @pending_courses = @user.courses.where(:status => "proposal")
+    @pending_courses  = @user.courses.where(:status => "proposal")
   end
 
   def profile_past_taught
@@ -28,11 +27,10 @@ class UsersController < ApplicationController
 
   def profile_suggest
     @top_suggestions =  Suggestion.tally(
-       {  :at_least => 1,
-           :at_most => 10000,
-           :limit => 100,
-           :order => "suggestions.name ASC"
-       })
+        :at_least => 1,
+        :at_most  => 10000,
+        :limit    => 100,
+        :order    => "suggestions.name ASC")
       @suggestions = (@top_suggestions & @suggestions_in_my_location).paginate(:page => params[:page], :per_page => 6)
   end
 
