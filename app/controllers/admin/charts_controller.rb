34a4ed3d7f0750d7982c676a1dd941e9aa07f485
Ruby Ku <rubyku85@gmail.com>
2011-12-11 23:17:41 -0600

    changes to layout & admin

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -5,17 +5,30 @@ class Admin::ChartsController < Admin::AdminController
     @users    = User.order('DATE(created_at) DESC').group("DATE(created_at)").count
     @courses  = Course.unscoped.order('DATE(created_at) DESC').group("DATE(created_at)").count
     @students = Role.where(:role => 'student').order('DATE(created_at) DESC').group("DATE(created_at)").count
+    
+    
+    # Per month
+    @users_by_month = User.order('MONTH(created_at) DESC').group("MONTH(created_at)").count
+    @courses_by_month = Course.group("MONTH(created_at)").count
+    @students_by_month = Role.where(:role => 'student').group("MONTH(created_at)").count
 
     # Week over week calculations
     wow_users = @users.clone.to_a
     user_count_last_week        = wow_users.shift(7).sum {|date, count| count}
 
     # Totals
-    @revenue        = Payment.select('SUM(amount) as sum').first.sum.to_f
+    @transaction    = Payment.select('SUM(amount) as sum').first.sum.to_f
     @total_users    = User.count
     @total_courses  = Course.count
     @total_students = Role.where(:role => 'student').count
+    @total_teachers = Role.where(:role => 'teacher').count
     @estimated_user_count_six_months = (user_count_last_week * 4 * 6) + @total_users
+    
+    #
+    @paying_courses = Course.where('price != 0').count
+    @free_courses   = Course.where('price = 0').count
+    @amazon_fees    = @transaction * 0.029 + 0.3
+    @teachers_share = @transaction * 0.85
   end
 
 end
