934278d35c0ea563faf2c0c45c7886f1e6217869
Ruby Ku <rubyku85@gmail.com>
2011-12-11 23:39:45 -0600

    admin

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -8,9 +8,9 @@ class Admin::ChartsController < Admin::AdminController
     
     
     # Per month  
-    @users_by_month = User.group("extract( month from DATE(created_at)) ").count
-    @courses_by_month = Course.group("MONTH(created_at)").count
-    @students_by_month = Role.where(:role => 'student').group("MONTH(created_at)").count
+    #@users_by_month = User.group("extract( month from DATE(created_at)) ").count
+    #@courses_by_month = Course.group("MONTH(created_at)").count
+    #@students_by_month = Role.where(:role => 'student').group("MONTH(created_at)").count
 
     # Week over week calculations
     wow_users = @users.clone.to_a
