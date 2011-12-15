0469d6968ee2afeee7545b265e2efcfa33de7b67
schneems <richard.schneeman@gmail.com>
2011-12-15 15:38:48 -0600

    Fix devise problem with sign-up wizard

--- app/controllers/users/wizard_controller.rb
+++ app/controllers/users/wizard_controller.rb
@@ -1,46 +1,66 @@
 class Users::WizardController < ApplicationController
-  
+
+  before_filter :authenticate_user!
+
   def show
     @user = current_user
     @step = params[:id].to_sym
-    @next_step = next_step(@step)
-    render @step
+    @next_step = next_step(@step)||:finish
+    render_step @step
   end
-  
+
   def update
     @user = current_user
     @step = params[:id].to_sym
     @next_step = next_step(@step)
     case @step
     when :confirm_password
-      @user.update_with_password(params[:user])
+      @user.update_attributes(params[:user])
     when :confirm_profile
       @user.update_attributes(params[:user])
     end
-    @user.save!
-    redirect_to_next(@next_step)
-  end  
-
-  private
-  def steps
-    [:confirm_password, :confirm_profile]
+    sign_in(@user, :bypass => true) # needed for devise
+    if @user.save
+      redirect_to_next(@next_step)
+    else
+      render_step @step
+    end
   end
 
-  def next_step(current_step)  
+  private
+
+  def render_step(step)
+    if step.nil? || step == :finish
+      redirect_to_finish
+    else
+      render step
+    end
+  end
+
+
+  def steps
+    [:confirm_password, :confirm_profile, :invite_fb]
+  end
+
+  def next_step(current_step)
     @index = steps.index(current_step)
-    if @index < steps.length
+    if @index.present? && @index < steps.length
       return steps.at(@index + 1)
     else
       return nil
     end
   end
- 
+
   def redirect_to_next(next_step)
     if next_step.nil?
       flash[:notice] = "User information successfully updated!"
-      redirect_to @user
+      redirect_to_finish
     else
-      redirect_to "/wizard/#{next_step}"
+      redirect_to wizard_path(next_step)
     end
+  end
+
+  def redirect_to_finish
+    redirect_to @user
   end
 end
