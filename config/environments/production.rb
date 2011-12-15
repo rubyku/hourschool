31c05e4e2945ad4fcc0377392488b7ff24831a5b
Ruby Ku <rubyku85@gmail.com>
2011-12-15 15:39:24 -0600

    updated email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ HourschoolV2::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'hourschool.com' }
+  config.action_mailer.default_url_options = { :host => 'greendoors.hourschool.com' }
   ### ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
