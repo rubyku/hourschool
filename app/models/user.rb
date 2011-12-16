19c3b36330faf18b5e095d6edb13bd867e3de621
schneems <richard.schneeman@gmail.com>
2011-12-15 18:08:12 -0600

    save facebook_id along with fb_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   #validate :supported_location, :location_format
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :location, :fb_token
-  attr_accessible :zipcode, :zip, :bio, :referral, :facebook, :twitter, :web
+  attr_accessible :zipcode, :zip, :bio, :referral, :facebook_id, :twitter_id, :web
 
   include MethodCacheable
   extend FriendlyId
