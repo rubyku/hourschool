cc1c0c1e5574c9669a737cfa90d76e50cf17e1cd
Ruby Ku <rubyku85@gmail.com>
2011-12-19 20:07:26 -0600

    sort filters on suggestions

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -14,7 +14,7 @@ class SuggestionsController < ApplicationController
            :limit => 100,
            :order => "suggestions.name ASC"
        })
-    @suggestions = (@top_suggestions & @suggestions_in_my_location).paginate(:page => params[:page], :per_page => 6)
+    @suggestions = (@top_suggestions & @suggestions_in_my_location).paginate(:page => params[:page], :per_page => 20)
     if !params[:order].nil?
       order_suggestions(params[:order])
     end
