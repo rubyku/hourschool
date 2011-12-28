1bfcfe2b43f2ba0428ff0d4f4ffef1870c8860d9
schneems <richard.schneeman@gmail.com>
2011-12-28 12:02:44 -0600

    ask for friends_location from facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,9 +198,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   if Rails.env == "development"
-    config.omniauth :facebook, "116183295130221", "83d6e8f67cf05b1710d073ac1af99a70", {:scope => 'publish_stream,offline_access,email,user_about_me,user_location'}
+    config.omniauth :facebook, "116183295130221", "83d6e8f67cf05b1710d073ac1af99a70", {:scope => 'publish_stream,offline_access,email,user_about_me,user_location,friends_location'}
   else
-    config.omniauth :facebook, "144671482261988", "a7f47f266872b6ce3847c9b7720bcdf4", {:scope => 'publish_stream,offline_access,email,user_about_me,user_location'}
+    config.omniauth :facebook, "144671482261988", "a7f47f266872b6ce3847c9b7720bcdf4", {:scope => 'publish_stream,offline_access,email,user_about_me,user_location,friends_location'}
   end
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
