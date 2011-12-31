9e5d6c0e3a5a00fef8aa6be9a7a723e8edade979
schneems <richard.schneeman@gmail.com>
2011-12-31 01:46:18 -0600

    change admin auth method

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,8 +1,4 @@
 class Admin::AdminController < ApplicationController
-  before_filter :ensure_admin
-  
-  def ensure_admin
-    redirect_to root_path if current_user.blank? || !current_user.admin?
-  end
-  
+  before_filter :authenticate_admin!
+
 end
