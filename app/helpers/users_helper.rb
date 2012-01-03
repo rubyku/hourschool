4d593d96b54ca02198f4d0da08aac53d0c55f227
Ruby Ku <rubyku85@gmail.com>
2012-01-03 11:47:51 -0600

    randomize hp images & user avatars on course sidebar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,21 @@
 module UsersHelper
 
-  def avatar_for(user)
-    if @user.photo.exists?
-      return image_tag(@user.photo.url(:small))
+  def avatar_for(user, options = {})
+    options[:class]||="avatar-big"
+    if user.photo.exists?
+      return image_tag(user.photo.url(:small), options)
     else
       avatar_url = ['/images/v2/Avatars_01_V1.png',
                     '/images/v2/Avatars_02_V1.png',
                     '/images/v2/Avatars_03_V1.png',
                     '/images/v2/Avatars_04_V1.png',
                     '/images/v2/Avatars_05_V1.png' ].sample
-      return image_tag(avatar_url, :class => "avatar-big")
+      return image_tag(avatar_url, options)
     end
   end
 
+  def link_to_avatar(user, options = {})
+     link_to avatar_for(user, options), user
+  end
+  
 end
