aaeb2c68b9adf151e4f7c29407007748b23472ad
Ruby Ku <rubyku85@gmail.com>
2012-01-16 17:36:30 -0600

    paginate at 9 courses
    Conflicts:
    
    	app/controllers/courses/browse_controller.rb

--- app/controllers/courses/browse_controller.rb
+++ app/controllers/courses/browse_controller.rb
@@ -1,7 +1,7 @@
 class Courses::BrowseController < ApplicationController
 
   def index
-    @courses = Course.active.paginate(:page => params[:page])
+    @courses = Course.active.order(:date, :created_at).paginate(:page => params[:page], :per_page => 9)
     if current_user
       @no_courses_in_user_city = Course.joins(:city).where("cities.name = '#{current_user.city}'").count == 0
     else
@@ -27,6 +27,10 @@ class Courses::BrowseController < ApplicationController
     when 'tags'
        @courses = Course.active.tagged_with(params[:tag]).paginate(:page => params[:page])
     end
+    courses  = courses.located_in(params[:city])         if params[:city].present?
+    courses  = courses.tagged_with(params[:tag])         if params[:tag].present?
+    @courses = courses.paginate(:page => params[:page], :per_page => 9)
+    @params  = params
   end
 
 end
