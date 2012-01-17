9abfbb67eb5b9b93203df270550f205d0f372048
Ruby Ku <rubyku85@gmail.com>
2012-01-16 18:01:48 -0600

    paginate at 9

--- app/controllers/courses/browse_controller.rb
+++ app/controllers/courses/browse_controller.rb
@@ -2,7 +2,6 @@ class Courses::BrowseController < ApplicationController
 
   def index
     @courses = Course.active.order(:date, :created_at).paginate(:page => params[:page], :per_page => 9)
-    @courses = Course.active.order(:date, :created_at).paginate(:page => params[:page])
     if current_user
       @no_courses_in_user_city = Course.joins(:city).where("cities.name = '#{current_user.city}'").count == 0
     else
@@ -30,7 +29,6 @@ class Courses::BrowseController < ApplicationController
     courses  = courses.located_in(params[:city])         if params[:city].present?
     courses  = courses.tagged_with(params[:tag])         if params[:tag].present?
     @courses = courses.paginate(:page => params[:page], :per_page => 9)
-    @courses = courses.paginate(:page => params[:page])
     @params  = params
   end
 
