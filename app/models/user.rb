67c37e54b4320ecc0edf68ba1bfee5683400ee5d
schneems <richard.schneeman@gmail.com>
2012-01-17 14:38:53 -0600

    remove attr_accessible from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name, :email
   # Setup accessible (or protected) attributes for your model
-  #validate :supported_location, :location_format
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :location, :fb_token
-  attr_accessible :zipcode, :zip, :bio, :referral, :facebook_id, :twitter_id, :web
+  # validate :supported_location, :location_format
 
   include MethodCacheable
   extend FriendlyId
