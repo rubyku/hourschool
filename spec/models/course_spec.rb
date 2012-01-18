b7076f9cd16058da7f41ea44f65c40d3925e13d6
schneems <richard.schneeman@gmail.com>
2012-01-18 11:58:56 -0600

    get correct test duration

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe Course do
   describe 'time' do
+    after(:each) do
+      Timecop.return
+    end
+
     let(:cst) { TZInfo::Timezone.get('America/Chicago')     }
     let(:pst) { TZInfo::Timezone.get('America/Los_Angeles') }
-    let(:now) { Time.zone.local(2010, 6, 1, 17, 0, 0)       }
+    let(:stub_time) { Time.zone.local(2010, 6, 1, 17, 0, 0)       }
 
     it 'active? will be true when the class is viewed from the same time zone' do
-      Timecop.freeze(now) do
+      Timecop.freeze(stub_time) do
         Time.zone = cst
         course    = Factory.stub(:course, :date => Date.today)
         Timecop.travel(1.hour)
