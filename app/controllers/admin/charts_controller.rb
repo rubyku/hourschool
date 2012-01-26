91b35ee3b21c3f7ea3882812f48f7aa93ae5b7bd
Ruby Ku <rubyku85@gmail.com>
2012-01-26 00:17:59 -0600

    updating metrics

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -10,30 +10,33 @@ class Admin::ChartsController < Admin::AdminController
     @teachers = User.joins(:roles).where("roles.name = 'teacher'").order('DATE(roles.created_at) DESC')
 
     # Per month
-    @users_by_month = User.order('extract( month from DATE(created_at)) DESC').group("extract( month from DATE(created_at)) ").count
-    @courses_by_month = Course.unscoped.group("extract( month from DATE(created_at)) ").count
-    @courses_by_month_austin = Course.unscoped.joins(:city).where("cities.name = 'Austin'").group("extract( month from DATE(courses.created_at)) ").count
-    @students_by_month = Role.where(:name => 'student').group("extract( month from DATE(created_at)) ").count
-    @transaction_by_month = Payment.group("extract( month from DATE(created_at))").sum('amount')
-
+    @users_by_month             = User.order('extract( month from DATE(created_at)) DESC').group("extract( month from DATE(created_at)) ").count
+    @courses_by_month           = Course.unscoped.group("extract( month from DATE(created_at)) ").count
+    @courses_by_month_austin    = Course.unscoped.joins(:city).where("cities.name = 'Austin'").group("extract( month from DATE(courses.created_at)) ").count
+    @students_by_month          = Role.where(:name => 'student').group("extract( month from DATE(created_at)) ").count
+    @transaction_by_month       = Payment.group("extract( month from DATE(created_at))").sum('amount')
+    @transaction_count_by_month = Payment.group("extract( month from DATE(created_at))").count
+    # @amazon_fees_by_month       = @transaction_by_month * 0.029 + @transaction_count_by_month * 0.3
+    # @teachers_share_by_month    = @transaction_by_month * 0.85
+    # @revenue_by_month           = @transaction_by_month - @amazon_fees_by_month - @teachers_share_by_month
 
     # Week over week calculations
     wow_users = @users.clone.to_a
     user_count_last_week        = wow_users.shift(7).sum {|date, count| count}
 
     # Totals
-    @transaction    = Payment.select('SUM(amount) as sum').first.sum.to_f
-    @total_users    = User.count
-    @total_fb_users = User.where("fb_token is not null").count
-    @total_courses  = Course.count
-    @total_students = Role.where(:name => 'student').count
-    @total_teachers = Role.where(:name => 'teacher').count
-    @estimated_user_count_six_months = (user_count_last_week * 4 * 6) + @total_users
+    @total_transaction          = Payment.select('SUM(amount) as sum').first.sum.to_f
+    @total_transaction_count    = Payment.count
+    @total_users                = User.count
+    @total_fb_users             = User.where("fb_token is not null").count
+    @total_courses              = Course.count
+    @total_students             = Role.where(:name => 'student').count
+    @total_teachers             = Role.where(:name => 'teacher').count
 
     @paying_courses = Course.where('price != 0').count
     @free_courses   = Course.where('price = 0').count
-    @amazon_fees    = @transaction * 0.029 + 0.3
-    @teachers_share = @transaction * 0.85
+    @amazon_fees    = @total_transaction * 0.029 + @total_transaction_count * 0.3
+    @teachers_share = @total_transaction * 0.85
   end
 
 
