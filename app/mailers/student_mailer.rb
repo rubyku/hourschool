8a775d8ec818fac79fc307ad37a996592287f90f
schneems <richard.schneeman@gmail.com>
2012-01-30 14:28:58 -0600

    set time_zone

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -4,24 +4,28 @@ class StudentMailer < ActionMailer::Base
   def send_invite_friends_mail(student, course)
     @student = student
     @course = course
+    Time.zone = @student.time_zone
     mail(:to => @student.email, :subject => "The class you signed up for is 3 days away and needs more students!")    
   end
   
   def send_positive_confirmation(student, course)
     @student = student
     @course = course
+    Time.zone = @student.time_zone
     mail(:to => @student.email, :subject => "The class you signed up for is tomorrow!")
   end
   
   def send_negative_confirmation(student, course)
     @student = student
     @course = course
+    Time.zone = @student.time_zone
     mail(:to => @student.email, :subject => "The class you signed up for has been canceled")    
   end
 
   def send_post_class_feedback(student, course)
     @student = student
     @course = course
+    Time.zone = @student.time_zone
     mail(:to => @student.email, :subject => "Let us know how class went!")      
   end
   
