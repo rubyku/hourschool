8f39f9611599c0a4602b5fc32595bc9db7719a0e
schneems <richard.schneeman@gmail.com>
2012-02-02 15:08:51 -0600

    try to get remember me working for Facebook signup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,16 +6,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user.update_facebook_from_oauth(env["omniauth.auth"])
       cookies[:ignoreFacebookReconnect] = true
     else
-      # You need to implement the method below in your model
       @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
     end
-
-    sign_in(@user, :bypass => true) # needed for devise
-    @user.remember_me!
+    # sign_in(@user, :bypass => true) # needed for devise
+    # @user.remember_me!
 
     if @user.zip.present?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      redirect_to  after_sign_in_path_for @user
+      @user.remember_me = true
+      sign_in_and_redirect @user, :event => :authentication
     else
       flash[:notice] = "Thanks for signing up!"
       redirect_to after_register_path(:confirm_password)
