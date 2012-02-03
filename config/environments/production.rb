4a4f7f7b012839808a4c48d30b4fa918f8db966e
schneems <richard.schneeman@gmail.com>
2012-02-03 16:00:25 -0600

    tell assets to use digest in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,9 @@ HourschoolV2::Application.configure do
   config.serve_static_assets = false
   config.action_controller.asset_host = "http://d1ufsa5oa5lbkj.cloudfront.net"
 
-  # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.static_cache_control = "public, max-age=2592000"
+  config.assets.digest = true
+
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
