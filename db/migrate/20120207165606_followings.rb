88c30db96a87d86b8faa100b516e3cf83e559542
schneems <richard.schneeman@gmail.com>
2012-02-08 13:19:40 -0600

    related migration

---
+++ db/migrate/20120207165606_followings.rb
@@ -0,0 +1,19 @@
+class Followings < ActiveRecord::Migration
+  def up
+    create_table :followings do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+      t.string  :status
+      t.string  :relationship
+      t.timestamps
+    end
+
+
+    add_index :followings, [:follower_id, :followed_id], :unique => true
+    add_index :followings, :followed_id
+  end
+
+  def down
+    drop_table :followings
+  end
+end
