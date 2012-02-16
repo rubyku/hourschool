654d72cd22dc076831b3ae53dc79ac48cc445caa
Ruby Ku <rubyku85@gmail.com>
2012-02-16 01:10:30 -0600

    temp disabling user reg emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -47,11 +47,11 @@ class UserMailer < ActionMailer::Base
     mail(:to => "ruby@hourschool.com, alex@hourschool.com", :subject => "#{@user.name} submitted a feedback form!")
   end
 
-  def send_registration_mail(user_email,user_name)
-    @email = user_email
-    @name = user_name
-    mail(:to => user_email, :subject => "Welcome to HourSchool!")
-  end
+  # def send_registration_mail(user_email,user_name)
+  #   @email = user_email
+  #   @name = user_name
+  #   mail(:to => user_email, :subject => "Welcome to HourSchool!")
+  # end
 
    def send_course_registration_mail(user_email, user_name, course)
      @email = user_email
