0a82f55a76545ba387feb2c9295945a58472b10b
schneems <richard.schneeman@gmail.com>
2012-02-20 13:18:09 -0600

    ensure no users can create an account or login to an account unless the email is valid for the enterprise

--- spec/acceptance/accounts/sign_up.rb
+++ spec/acceptance/accounts/sign_up.rb
@@ -6,19 +6,32 @@ feature "Acceptance Accounts Sign Up" do
 
 
   describe 'a restricted Account' do
-    scenario 'requires specific email' do
-        geo_name_web_mock
-        user_count = User.count
-        visit new_user_registration_url(:subdomain => account.subdomain)
-        fill_in  'user_name',     :with => user_attributes[:name]
-        fill_in  'user_zipcode',  :with => user_attributes[:zip]
-        fill_in  'user_email',    :with => "not_valid_account@nope.com"
-        fill_in  'user_password', :with => user_attributes[:password]
-        fill_in  'user_password_confirmation', :with => user_attributes[:password]
-        click_on 'user_submit'
-        save_and_open_page
-        ## user count should not change
-        User.count.should == user_count
-      end
+    scenario 'fails unless has a specific email' do
+      geo_name_web_mock
+      user_count = User.count
+      visit new_user_registration_url(:subdomain => account.subdomain)
+      fill_in  'user_name',     :with => user_attributes[:name]
+      fill_in  'user_zipcode',  :with => user_attributes[:zip]
+      fill_in  'user_email',    :with => "not_valid_account@nope.com"
+      fill_in  'user_password', :with => user_attributes[:password]
+      fill_in  'user_password_confirmation', :with => user_attributes[:password]
+      click_on 'user_submit'
+      ## user count should not change
+      User.count.should == user_count
+    end
+
+    scenario 'passes when correct email is used' do
+      geo_name_web_mock
+      user_count = User.count
+      visit new_user_registration_url(:subdomain => account.subdomain)
+      fill_in  'user_name',     :with => user_attributes[:name]
+      fill_in  'user_zipcode',  :with => user_attributes[:zip]
+      fill_in  'user_email',    :with => "foo_#{Time.now.to_i}@#{account.subdomain}.com"
+      fill_in  'user_password', :with => user_attributes[:password]
+      fill_in  'user_password_confirmation', :with => user_attributes[:password]
+      click_on 'user_submit'
+      ## user count should change
+      User.count.should > user_count
+    end
   end
 end
