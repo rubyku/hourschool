d649f2ea91ed0a8982348bb679b9d7fd58b8185a
Chap Ambrose <chapambrose@gmail.com>
2012-03-20 10:46:52 -0500

    allow users who register on non-community sites to have memberships with the account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,8 +6,26 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  # override create so we can create account membership
+  # if on a non-community site
   def create
-    super
+    build_resource
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        Membership.create!(:user => resource, :account => current_account) if current_account
+        respond_with resource, :location => redirect_location(resource_name, resource)
+      else
+        set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render_with_scope :new }
+    end
   end
 
   def update
