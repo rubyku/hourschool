8de0e88da3354ba6f85358697c2855d92de98839
schneems <richard.schneeman@gmail.com>
2012-03-22 16:27:35 -0400

    i botched the routes in a rebase, fixing this

--- config/routes.rb
+++ config/routes.rb
@@ -125,7 +125,7 @@ HourschoolV2::Application.routes.draw do
 
   match '/start'                      => 'pages#index'
 
-
+  post 'courses/:id/duplicate'        => 'courses#duplicate', :as => 'duplicate_course'
 
   root :to                            => "pages#index"
 
