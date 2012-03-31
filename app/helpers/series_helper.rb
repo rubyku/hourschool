65ffcdeab0e6fe76c9a5d06977ff8994cd7bb818
schneems <richard.schneeman@gmail.com>
2012-03-31 17:10:30 -0500

    convert time to utc before checking

--- app/helpers/series_helper.rb
+++ app/helpers/series_helper.rb
@@ -1,6 +1,6 @@
 module SeriesHelper
   def checked_for_series_if(series, day)
-    check_day = Time.now.next_week(day).in_time_zone
+    check_day = Time.now.next_week(day).in_time_zone.utc
     series.schedule.occurs_on?(check_day)
   end
 end
