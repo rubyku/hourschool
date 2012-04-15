c0f432328826da2da9b5530fccc8f61783472c43
Ruby Ku <rubyku85@gmail.com>
2012-04-14 22:47:09 -0500

    wrong rails class on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
       # @featured_courses = Course.where(:featured => true).order("created_at desc").first(4)
       @fav2 = Course.where(:id => 392).first || Course.live.random.first
       @fav1 = Course.where(:id => 421).first || Course.live.random.first
-      @fav3 = Course.where(:id => 188).first || Course.live.random.first
+      @fav3 = Course.where(:id => 438).first || Course.live.random.first
       @fav4 = Course.where(:id => 384).first || Course.live.random.first
       render :layout => 'home'
     else
