08e443c227ddf1a4180bf6ade25897e2ea5e2f77
schneems <richard.schneeman@gmail.com>
2012-05-05 11:50:19 -0500

    show edit action

--- app/controllers/courses/series_controller.rb
+++ app/controllers/courses/series_controller.rb
@@ -14,17 +14,17 @@ class Courses::SeriesController < ApplicationController
     @series ||= Series.create(:courses => [@course], :name => @course.name)
     @series.update_attributes(series_params)
     flash[:notice] = "successfully scheduled: #{@course.name}"
-    redirect_to edit_courses_series_path(@series)
+    render :action => "edit"
   end
 
   def update
     @series = current_user.series.find(params[:id])
     @series.update_attributes(series_params)
-    redirect_to edit_courses_series_path(@series)
+    render :action => "edit"
   end
 
   private
   def series_params
-    {:schedule => params[:schedule]}.merge(params[:series]||{})
+    {:schedule => params[:schedule] }.merge(params[:series]||{})
   end
 end
