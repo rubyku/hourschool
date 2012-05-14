5bedbbe9634ab57ac830727e82ac8ab6e0c2773d
Ruby Ku <rubyku85@gmail.com>
2012-05-14 16:38:53 -0500

    after sign up, go to /learn instead of profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -45,7 +45,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
     def after_update_path_for(resource)
-      "/profile"
+      "/learn"
     end
 
     def after_sign_up_path_for(resource)
