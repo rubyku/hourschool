1ecb9d39740729346f4938dd8968f5f8dcb34557
Ruby Ku <rubyku85@gmail.com>
2012-05-14 14:52:31 -0500

    adding photo column to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120411164450) do
+ActiveRecord::Schema.define(:version => 20120511221259) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
     t.string   "subdomain"
     t.string   "email_regex"
     t.text     "description"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
-    t.boolean  "private",     :default => false, :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
+    t.boolean  "private",            :default => false, :null => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "accounts", ["subdomain"], :name => "index_accounts_on_subdomain", :unique => true
