450ef4d5185fc07710334075bbef79983ed76f59
Ruby Ku <rubyku85@gmail.com>
2012-05-15 12:30:43 -0500

    don't show account classes in /learn yet

--- app/models/account.rb
+++ app/models/account.rb
@@ -21,7 +21,7 @@ class Account < ActiveRecord::Base
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
   def self.public_ids
-    self.open.collect(&:id) + [0] - [6]
+    self.open.collect(&:id) + [0] - [1] - [2] - [3] - [4]
   end
 
   private
