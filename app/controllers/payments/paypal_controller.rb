24d4b741b80c1a01e8042b8018598ea1f2e97837
Ruby Ku <rubyku85@gmail.com>
2012-05-17 18:00:19 -0500

    wdb still doesn't work

--- app/controllers/payments/paypal_controller.rb
+++ app/controllers/payments/paypal_controller.rb
@@ -2,19 +2,11 @@ class Payments::PaypalController < ApplicationController
 
 
   def paypal
-    if current_account == Account.find(4)
-       Paypal::Express::Request.new(
-        :username   => christina.mirando_api1.gmail.com,
-        :password   => 3TRLAHE65P89ZFXG,
-        :signature  => AHuWaRANd0mMqR6W2dFx.KCc0SaRAgHmt8JBU3ShwwNs1HxhwmX0p7XT
-      )
-    else 
-       Paypal::Express::Request.new(
-        :username   => PAYPAL_USERNAME,
-        :password   => PAYPAL_PASSWORD,
-        :signature  => PAYPAL_SIGNATURE
-      )
-    end
+     Paypal::Express::Request.new(
+      :username   => PAYPAL_USERNAME,
+      :password   => PAYPAL_PASSWORD,
+      :signature  => PAYPAL_SIGNATURE
+    )
   end
   
   def request_for_course(course)
