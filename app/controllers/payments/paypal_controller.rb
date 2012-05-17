00653e16a29d88ae70164f773a10fcc312c0f42a
Ruby Ku <rubyku85@gmail.com>
2012-05-17 17:32:18 -0500

    one more try with wdb paypal

--- app/controllers/payments/paypal_controller.rb
+++ app/controllers/payments/paypal_controller.rb
@@ -2,13 +2,13 @@ class Payments::PaypalController < ApplicationController
 
 
   def paypal
-    if current_account == Account.find_by_subdomain("womendesignbuild")
+    if current_account == Account.find(4)
        Paypal::Express::Request.new(
         :username   => christina.mirando_api1.gmail.com,
         :password   => 3TRLAHE65P89ZFXG,
         :signature  => AHuWaRANd0mMqR6W2dFx.KCc0SaRAgHmt8JBU3ShwwNs1HxhwmX0p7XT
       )
-    elsif community_site? 
+    else 
        Paypal::Express::Request.new(
         :username   => PAYPAL_USERNAME,
         :password   => PAYPAL_PASSWORD,
