73e016c17025bb954c596d496341643662d78b2b
Ruby Ku <rubyku85@gmail.com>
2012-05-17 21:56:47 -0500

    changed mailer in development environment back to :test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ HourschoolV2::Application.configure do
   ### ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # A dummy setup for development - no deliveries, but logged
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
