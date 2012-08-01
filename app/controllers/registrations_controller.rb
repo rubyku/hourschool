b1d110f39757d9d6893e9ae1e5800d9fadd1aca1
Chap Ambrose <chapambrose@gmail.com>
2012-08-01 16:52:53 -0500

    fix for updating credit card info

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -32,9 +32,17 @@ class RegistrationsController < Devise::RegistrationsController
   def update
     resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     if params[:stripeToken].present?
-      stripe_customer = resource.stripe_customer
-      stripe_customer.card = params[:stripeToken]
-      stripe_customer.save
+      if resource.stripe_customer
+        stripe_customer = resource.stripe_customer
+        stripe_customer.card = params[:stripeToken]
+        stripe_customer.save
+      else
+        resource.create_stripe_customer(
+          :card => params[:stripeToken],
+          :description => "user_#{resource.id}",
+          :email => resource.email
+        )
+      end
     end
 
     # Override Devise to use update_attributes instead of update_with_password.
