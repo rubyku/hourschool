bcd5e72ff19df63351562434df7e0cc9e33cc376
Ruby Ku <rubyku85@gmail.com>
2012-07-31 21:42:58 -0500

    dont redirect FB sign in to wizard

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,18 +13,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     session["devise.facebook_data"] = request.env["omniauth.auth"]
     @user.remember_me = true
     @user.save
+    sign_in_and_redirect @user, :event => :authentication
 
     # sign_in(@user, :bypass => true) # needed for devise
     # @user.remember_me!
 
     # if a user doesn't have a zipcode (new users) send them to the wizard (after_register_path)
-    if @user.zip.present?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      sign_in(@user, :event => :authentication)
-      redirect_to after_register_path(:confirm_password)
-    end
+    # if @user.zip.present?
+    #   flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+    #   sign_in_and_redirect @user, :event => :authentication
+    # else
+    #   sign_in(@user, :event => :authentication)
+    #   redirect_to after_register_path(:confirm_password)
+    # end
   end
 
   def passthru
