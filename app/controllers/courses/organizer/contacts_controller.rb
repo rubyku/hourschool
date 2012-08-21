bdcb8bdd1d25fc3fa336a95cca6563961901ae92
Ruby Ku <rubyku85@gmail.com>
2012-08-21 18:32:51 -0400

    user not logged in can send message to teacher

--- app/controllers/courses/organizer/contacts_controller.rb
+++ app/controllers/courses/organizer/contacts_controller.rb
@@ -1,14 +1,18 @@
 # courses/3/organizer/contacts
 # courses/:course_id
 class Courses::Organizer::ContactsController < ApplicationController
-  
+
   def new
     @course = Course.find(params[:course_id])
   end
 
   def create
     @course = Course.find(params[:course_id])
-    UserMailer.contact_teacher(current_user, @course, params[:message]).deliver
+    if current_user.blank?
+      UserMailer.contact_teacher_from_nonuser(params[:messenger_email], @course, params[:message]).deliver
+    else
+      UserMailer.contact_teacher(current_user, @course, params[:message]).deliver
+    end
     flash[:notice] = "Your message has successfully been sent"
     redirect_to @course
   end
