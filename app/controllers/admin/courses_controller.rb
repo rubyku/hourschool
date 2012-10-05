bec3df84b725093ccefb9c6f810780a9ccaf3260
Ruby Ku <rubyku85@gmail.com>
2012-10-05 16:15:51 -0400

    updated admin course page

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -2,16 +2,21 @@ class Admin::CoursesController < Admin::AdminController
   layout 'application'
 
   def index
-    
+
     @courses = current_account ? current_account.courses.order('DATE(date) DESC').where(:status => "live") : Course.order('DATE(date) DESC').where(:status => "live")
-      
+
     @proposals = current_account ? current_account.courses.order('DATE(created_at) DESC').where(:status => "proposal") : Course.order('DATE(created_at) DESC').where(:status => "proposal")
-   
+
     @approved = current_account ? current_account.courses.order('DATE(created_at) DESC').where(:status => "approved") : Course.order('DATE(created_at) DESC').where(:status => "approved")
-    
+
     @user = current_user
-  
+
   end
+
+  def test
+    @courses = current_account ? current_account.courses.order('DATE(date) DESC').where(:status => "live") : Course.order('DATE(date) DESC').where(:status => "live")
+  end
+
 
   def show
     @course = Course.find(params[:id])
@@ -22,9 +27,9 @@ class Admin::CoursesController < Admin::AdminController
     @course.update_attribute :status, "approved"
 
     #send email and other stuff here to the teacher
-    if @course.account.nil? 
+    if @course.account.nil?
       current_account = nil
-    else 
+    else
       current_account = @course.account
     end
     UserMailer.send_course_approval_mail(@course.teacher.email, @course.teacher.name, @course, current_account).deliver
