89906b135cfda882934f7cec4cdf83f726bb6e23
Ruby Ku <rubyku85@gmail.com>
2013-01-14 21:07:25 -0600

    updated invite email and custom landing page

--- app/controllers/users/ticket_invite_controller.rb
+++ app/controllers/users/ticket_invite_controller.rb
@@ -4,33 +4,35 @@ class Users::TicketInviteController < ApplicationController
   ## Person accepting the invit hits this
   def edit
     @user = User.where(:invite_token => params[:id], :status => "invitee").first
+    @role = @user.roles.first
+    @inviter = User.where(:id => @role.invite_user_id).first
+    @course = @role.course
   end
-
 
   ## Person accepting the invit hits this
   def update
-    user = current_user.extra_ticket_users.where(:invite_token => params[:id], :status => "invitee").first
-    role = user.roles.first
+    @user = User.where(:invite_token => params[:id], :status => "invitee").first
+    @role = @user.roles.first
     if current_user
-      role.user_id = current_user.id
-      role.save
-      user.destroy
+      @role.user_id = current_user.id
+      @role.save
+      @user.delete
     else
-      user.update_attributes(params[:user])
-      user.status = "claimed"
-      user.save
+      @user.update_attributes(params[:user])
+      @user.status = "claimed"
+      @user.save
     end
-    redirect_to role.course, :notice => "Ticket claimed successfully!!"
+    redirect_to @role.course, :notice => "Ticket claimed successfully!"
   end
 
   ## Person creating the invites hits this
   def create
     ## Duplicate email path
-    user = current_user.extra_ticket_users.where(:invite_token => params[:id], :status => "invitee").first
-    user.update_attributes(:email => params[:user][:email], :name => params[:user][:name])
+    @user = User.where(:invite_token => params[:id], :status => "invitee").first
+    @user.update_attributes(:email => params[:user][:email], :name => params[:user][:name])
 
-    role = user.roles.first
-    UserMailer.ticket_invite(:user => user, :role => role, :inviter => current_user).deliver
+    @role = @user.roles.first
+    UserMailer.ticket_invite(:user => @user, :role => @role, :inviter => current_user).deliver
     redirect_to :back
   end
 end
