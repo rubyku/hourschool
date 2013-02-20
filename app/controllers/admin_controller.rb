3751e876c4f62fea67301fe8674050e09cc4bfe4
Ruby Ku <rubyku85@gmail.com>
2013-02-20 11:02:52 -0600

    errand-jan2013

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,7 +19,7 @@ class AdminController < ApplicationController
 
   def show
     @users_sincelastnewsletter  = User.where("created_at > ?", '2012-05-31')
-    @courses_last_month         = Course.where("extract( month from DATE(starts_at)) = 12").where("extract( year from DATE(starts_at)) = 2012").where('price != 0').where(:status => "live")
+    @courses_last_month         = Course.where("extract( month from DATE(starts_at)) = 1").where("extract( year from DATE(starts_at)) = 2013").where('price != 0').where(:status => "live")
 
     render "admin/show/#{params[:id]}"
   end
