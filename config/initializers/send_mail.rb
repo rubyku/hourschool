5a7a2aa3e245bce3c142b5dc488ecb280035c3ce
schneems <richard.schneeman@gmail.com>
2016-04-25 11:55:23 -0500

    Secure email env

--- config/initializers/send_mail.rb
+++ config/initializers/send_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  
+
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "hourschool.com",
   :user_name            => "hello@hourschool.com",
-  :password             => "ac4dlove",
+  :password             => ENV['EMAIL_PASSWORD'],
   :authentication       => "plain"
   # :tls => true,
   #   :enable_starttls_auto => true,
